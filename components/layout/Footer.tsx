b133d36fbfac2b425ba9d691e2c78d0c918e53c2
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-16 23:46:58 +0000

    Create Footer.tsx

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-100 py-6 text-center text-sm text-gray-500">
+      &copy; 2025 ALX Listings. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
